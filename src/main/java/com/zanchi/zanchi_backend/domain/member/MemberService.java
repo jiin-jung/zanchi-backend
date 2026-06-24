4a2f96a267815bc1aa4424b9246449c3a1b6728c
jiin-jung <cki08543@gmail.com>
2026-06-24 10:40:49 +0900

    feat: 비밀번호 BCrypt 암호화 적용

--- src/main/java/com/zanchi/zanchi_backend/domain/member/MemberService.java
+++ src/main/java/com/zanchi/zanchi_backend/domain/member/MemberService.java
@@ -3,6 +3,7 @@ package com.zanchi.zanchi_backend.domain.member;
 import com.zanchi.zanchi_backend.domain.member.dto.ChangeNameResponse;
 import com.zanchi.zanchi_backend.web.member.form.MemberForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import java.util.List;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void signup(MemberForm form) {
         if (memberRepository.findByLoginId(form.getLoginId()).isPresent()) {
@@ -22,7 +24,7 @@ public class MemberService {
         Member member = Member.builder()
                 .name(form.getName())
                 .loginId(form.getLoginId())
-                .password(form.getPassword()) //지인아 여기 암호화 해야해 나중에 시간나면 하자
+                .password(passwordEncoder.encode(form.getPassword()))
                 .build();
 
         memberRepository.save(member);
